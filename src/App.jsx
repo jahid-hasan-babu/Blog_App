be305c07c07129f7ec12d66e08e07468ad876067
jahid-hasan-babu <jahidhasanbabu7821@gmail.com>
2023-11-17 01:25:32 +0600

    module14assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import HomePage from "./Pages/HomePage"
-import ByCategory from "./Pages/ByCategory"
-import Details from "./Pages/Details"
+import React from 'react'
+import Navbar from './layout/Navbar'
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Team from './pages/Team';
+import Services from './pages/Services';
+import Projects from './pages/Projects';
+import Testimonials from './pages/Testimonials';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage/>}/>
-        <Route path="/byCategory/:categoryID" element={<ByCategory/>}/>
-        <Route path="/details/:postID" element={<Details/>}/>
-      </Routes>
-    </BrowserRouter>
+    
+      <BrowserRouter>
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/team' element={<Team/>}/>
+        <Route path='/service' element={<Services/>}/>
+        <Route path='/project' element={<Projects/>}/>
+        <Route path='/testimonials' element={<Testimonials/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+       </Routes>
+      </BrowserRouter>
+    
   )
 }
 
